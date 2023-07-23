99ba198456bb2b8bbeba9b316a38897f2b964988
kylee <giseggi57@gmail.com>
2023-07-23 16:39:21 +0900

    initial commit

---
+++ src/main/java/com/gsg/lol_analyzer/LolAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.gsg.lol_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolAnalyzerApplication.class, args);
+	}
+
+}
