82b50edce078d892dc9c68c610db041cde70f5bb
kylee <giseggi57@gmail.com>
2023-08-06 17:21:42 +0900

    add MatchRecord

---
+++ src/main/java/com/gsg/lol_analyzer/entity/MatchRecord.java
@@ -0,0 +1,21 @@
+package com.gsg.lol_analyzer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class MatchRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String matchId;
+
+    private String processedCode;
+
+}
